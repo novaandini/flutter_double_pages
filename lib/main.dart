6872e70a931eb90d4ef8ebb100ae2eedb4847f07
novaandini <andininovaaa@gmail.com>
2024-11-05 14:28:13 +0800

    menambahkan halaman detail produk

--- lib/main.dart
+++ lib/main.dart
@@ -30,44 +30,133 @@ class FirstPage extends StatefulWidget {
 }
 
 class _FirstPageState extends State<FirstPage> {
+  final List<Map<String, String>> products = [
+    {
+      'name': 'Josephie Basket',
+      'price': '100.000',
+      'description': 'this is product this is product this is product this is product this is product this is product this is product this is product',
+      'image': 'assets/459629637_358651527211621_1419974415724817847_n.jpg'
+    },
+    {
+      'name': 'Sunflower Bouquet',
+      'price': '135.000',
+      'description': 'this is product',
+      'image': 'assets/460554949_865769718984813_7035978913964065287_n.jpg'
+    },
+    {
+      'name': 'Andrea Basket',
+      'price': '110.000',
+      'description': 'this is product',
+      'image': 'assets/461587480_1530838240853209_2784004203875285535_n.jpg'
+    },
+    {
+      'name': 'Lily Bouquet',
+      'price': '120.000',
+      'description': 'this is product',
+      'image': 'assets/461415948_1247882473149752_1037329320162924743_n.jpg'
+    },
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("First Page"),
+        title: const Text("Catalog"),
         centerTitle: true,
         actions: [
           IconButton(
             onPressed: () {
-              Navigator.of(context).push(MaterialPageRoute(builder: (context)=> const SecondPage()));
+              // Navigator.of(context).push(MaterialPageRoute(builder: (context)=> SecondPage(product: null,)));
             },
-            icon: Icon(Icons.arrow_forward, size: 40,),
+            icon: Icon(
+              Icons.arrow_forward,
+              size: 40,
+            ),
           )
         ],
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: GridView.builder(
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2, 
-            crossAxisSpacing: 10.0,
-            mainAxisSpacing: 10.0,
-            childAspectRatio: 1.0, 
-          ),
-          itemCount: 4,
-          itemBuilder: (context, index) {
-            return Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Colors.blueAccent,
-              ),
-              child: IconButton(
-                onPressed: () { Navigator.of(context).push(MaterialPageRoute(builder: (context)=> const SecondPage())); }, 
-                icon: Icon(Icons.add),
-              ),
-            );
-          },
-        ),
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              crossAxisSpacing: 10.0,
+              mainAxisSpacing: 10.0,
+              childAspectRatio: 0.75,
+            ),
+            itemCount: products.length,
+            itemBuilder: (context, index) {
+              final product = products[index];
+              return Card(
+                elevation: 4.0,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15.0),
+                ),
+                child: InkWell(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => SecondPage(product: product),
+                      ),
+                    );
+                  },
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Expanded(
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.vertical(
+                            top: Radius.circular(15.0),
+                          ),
+                          child: Image.asset(
+                            product['image']!,
+                            fit: BoxFit.cover,
+                            height: 200.0,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(10.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              product['name']!,
+                              style: TextStyle(
+                                fontSize: 16.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            SizedBox(height: 5.0),
+                            Text(
+                              product['description']!,
+                              style: TextStyle(
+                                fontSize: 14.0,
+                                color: Colors.grey[700],
+                              ),
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                            SizedBox(height: 5.0),
+                            Text(
+                              "Rp" + product['price']!,
+                              style: TextStyle(
+                                fontSize: 14.0,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                              ),
+                              maxLines: 2,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            }),
       ),
     );
   }
--- lib/second_page.dart
+++ lib/second_page.dart
@@ -1,20 +1,71 @@
 import 'package:flutter/material.dart';
 
-class SecondPage extends StatefulWidget {
-  const SecondPage({super.key});
+class SecondPage extends StatelessWidget {
+  final Map<String, String> product;
 
-  @override
-  State<SecondPage> createState() => _SecondPageState();
-}
-
-class _SecondPageState extends State<SecondPage> {
+  SecondPage({required this.product});
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Second Page"),
-        centerTitle: true,
+        title: Text("Detail Produk " + product['name']!),
       ),
+      body: Container(
+        padding: EdgeInsets.all(20.0),
+        height: 250.0,
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Image.asset(
+              product['image']!,
+              height: 250.0,
+              fit: BoxFit.cover,
+            ),
+            SizedBox(width: 10.0,),
+            Expanded(
+              child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                    product['name']!,
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                      fontSize: 32.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                ),
+                SizedBox(height: 5.0),
+                Text(
+                    product['description']!,
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      color: Colors.grey[500],
+                    ),
+                  ),
+                SizedBox(height: 5.0),
+                Text(
+                  "Rp" + product['price']!,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ],
+              ),
+            ),
+          ],
+        )
+      ),
+      
     );
+
   }
 }
